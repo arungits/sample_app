650da13458c409f335d2e26f9ee2b24ed536e56b
achandrasekaran <achandrasekaran@groupon.com>
2013-03-17 21:57:13 +0530

    Made lots of changes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  describe "Get 'home'" do
+    it "should be successful" do
+    	get 'home'
+    	response.should be_success
+    end
+    #it "should have the right title" do
+    #	get 'home'
+    #	response.should have_selector("title", :content => "Ruby on Rails Tutorial | Home Page")
+    #end
+    it "should have non-blank body" do
+    	get 'home'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
 
+  describe "Get 'contact'" do
+    it "should be successful" do
+    	get 'contact'
+    	response.should be_success
+    end
+  end
+
+  describe "Get 'about'" do
+  	it "should be successful" do
+  		get 'about'
+  		response.should be_success
+  	end
+  end
 end
